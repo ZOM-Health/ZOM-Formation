b9312f5d88f1b111d9c223fb9dcb68f45f767f04
akash <sanje.witter@yazom.com>
2019-08-30 11:56:35 +0530

    added airdrop form

---
+++ Airdrop/zom_airdrop.php
@@ -0,0 +1,284 @@
+<!--INCLUDE CSS-->
+<?php include 'inc/includes_top.php'; ?>
+
+<body class="zoom_page_background"
+      style="background: url('<?php echo base_url('assets/yazom/img/header_oval.svg'); ?>'); padding-top: 0;">
+
+<div class="container-fluid">
+    <a class="navbar-brand" href="<?php echo base_url('/'); ?>">
+        <img src="<?php echo base_url('assets/yazom/img/zom_logo.png'); ?>" width="120" alt="YAZOM">
+    </a>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right" id="login-menu">
+            <li><a class="secure-your-future js-scroll-trigger" href="#zom-airdrop-form">Zom Airdrop Form</a></li>
+        </ul>
+    </div>
+</div>
+
+<!-- Begin page content -->
+<section role="main" id="airdrop-section-1" class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <h4 class="zom-airdrop">ZOM Airdrop</h4>
+            <p class="zom-tokens">
+                50,000 ZOM Tokens
+            </p>
+            <p class="each-participant">
+                Each participant may receive a total of 50 ZOMs.
+            </p>
+        </div>
+
+        <div class="col-md-10 col-md-offset-1">
+            <div class="airdrop-list-items">
+                <ol class="fa-ul zom-airdrop-list-items">
+                    <li>
+                        <span class="fa-li">
+                            <img src="<?php echo base_url('assets/yazom/img/icon/silver_edition_checklist.svg'); ?>">
+                        </span>
+                        ZOM is a cryptocurrency initiative to facilitate health and wellness coverage for consumers. ZOM’s
+                        native blockchain is Ethereum.
+                    </li>
+                    <li>
+                        <span class="fa-li">
+                            <img src="<?php echo base_url('assets/yazom/img/icon/silver_edition_checklist.svg'); ?>">
+                        </span>
+                        Its primary use involves the acquisition of PASSES that provide discounts at select partners ranging
+                        from 5% to 15%.
+                    </li>
+                    <li>
+                        <span class="fa-li">
+                            <img src="<?php echo base_url('assets/yazom/img/icon/silver_edition_checklist.svg'); ?>">
+                        </span>
+                        These PASSES are digital in nature and have a colour spectrum identifier that focuses on three
+                        categories: Medical, Fitness and Diet.
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Begin page content -->
+<section role="main" id="methods-for-participation" class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <h4 class="methods-for-participation-title">Methods for Participation</h4>
+            <div class="clearfix"></div>
+        </div>
+        <img src="<?php echo base_url('assets/yazom/img/abstract_silver_and_gold_editions.svg'); ?>"
+             style="width: 100%;" alt="IMG" class="img-responsive center-block"/>
+        <div class="clearfix"></div>
+        <div class="col-md-10 col-md-offset-1" id="zom_methods_box">
+            <div class="col-md-4">
+                <div class="box text-center">
+                    <div class="reliability_img">
+                        <img src="<?php echo base_url('assets/yazom/img/icon/method-telegram.svg'); ?>" width="80" alt="YAZOM">
+                    </div>
+                    <div class="edition-info">
+                        <p class="title-social">10 ZOMs</p>
+                        <p class="zom-social-text">
+                            Join the ZOM <a class="zom-social-link" href="https://t.me/joinchat/HKT0YA4c58z6bmsxNLkKrA" target="_blank">Telegram Channel</a> and share the message - “I advocate for ZOM!”, along with your ZOM ID.
+                        </p>
+                        <p class="zom-social-eg">Eg. I advocate for ZOM! ZOM-C-ID-0000.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="box text-center">
+                    <div class="reliability_img">
+                        <img src="<?php echo base_url('assets/yazom/img/icon/method-reddit.svg'); ?>" width="80" alt="YAZOM">
+                    </div>
+                    <div class="edition-info">
+                        <p class="title-social">15 ZOMs</p>
+                        <p class="zom-social-text">
+                            Follow the <a class="zom-social-link" href="https://www.reddit.com/r/zom/" target="_blank">ZOM Reddit page</a> and make a post titled - “I joined because I want better health and wellness for humanity”, along with your ZOM ID.
+                        </p>
+                        <p class="zom-social-eg">Eg. I joined because I want better health and wellness for humanity. ZOM-C-ID-0000.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="box text-center">
+                    <div class="reliability_img">
+                        <img src="<?php echo base_url('assets/yazom/img/icon/method-twitter.svg'); ?>" width="80" alt="YAZOM">
+                    </div>
+                    <div class="edition-info">
+                        <p class="title-social">15 ZOMs</p>
+                        <p class="zom-social-text">
+                            Follow the <a class="zom-social-link" href="https://www.twitter.com/yazomit" target="_blank">Yazom Twitter page</a> and tweet “#ZOM @Yazomit is health and wellness done right.”, along with your ZOM ID.
+                        </p>
+                        <p class="zom-social-eg">Eg. #ZOM is health and wellness done right. ZOM-C-ID-0000.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</section>
+
+<!-- Begin page content -->
+<section role="main" id="step-by-step" class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h2 class="step-by-step-title">Step-by-Step Guide for ZOM Airdrop</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1" id="silver_edition_offerings">
+            <h4 class="follow-these-steps">Follow these steps below as a guide:</h4>
+            <ul class="time-line">
+                <li>
+                    <div class="time-line-badge">1</div>
+                    <div class="time-line-panel">
+                        <div class="time-line-heading">
+                            <h4 class="time-line-title"><a class="js-scroll-trigger" href="#zom-airdrop-form">GO TO THE AIRDROP FORM</a></h4>
+                        </div>
+                        <div class="time-line-body">&nbsp;</div>
+                    </div>
+                </li>
+                <li>
+                    <div class="time-line-badge">2</div>
+                    <div class="time-line-panel">
+                        <div class="time-line-heading">
+                            <h4 class="time-line-title">SUBMIT YOUR EMAIL ADDRESS AND ETHEREUM WALLET ADDRESS.</h4>
+                        </div>
+                        <div class="time-line-body">
+                            <p>Please double-check to ensure that your details are accurate.</p>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="time-line-badge">3</div>
+                    <div class="time-line-panel">
+                        <div class="time-line-heading">
+                            <h4 class="time-line-title">CHECK YOUR EMAIL FOR MAIL PROVIDING YOUR ZOM AIRDROP ID.</h4>
+                        </div>
+                        <div class="time-line-body">
+                            <p>Example: ZOM-C-ID-0000</p>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="time-line-badge">4</div>
+                    <div class="time-line-panel">
+                        <div class="time-line-heading">
+                            <h4 class="time-line-title"><a class="js-scroll-trigger" href="#methods-for-participation">FOLLOW THE METHODS FOR PARTICIPATION OUTLINED IN THE SECTION ABOVE.</a></h4>
+                        </div>
+                        <div class="time-line-body">&nbsp;</div>
+                    </div>
+                </li>
+                <li>
+                    <div class="time-line-badge">5</div>
+                    <div class="time-line-panel">
+                        <div class="time-line-heading">
+                            <h4 class="time-line-title">YOU CAN RECEIVE A MAXIMUM OF 50 ZOMS.</h4>
+                        </div>
+                        <div class="time-line-body">&nbsp;</div>
+                    </div>
+                </li>
+            </ul>
+            <p class="bonus-zoms">*Bonus ZOMs for community interaction and promotion is available. Valid while supplies last.</p>
+        </div>
+    </div>
+</section>
+
+<!-- Begin page content -->
+<section role="main" id="zom-airdrop-form" class="container-fluid">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="zom-airdrop-card">
+                <div class="box">
+                    <h1 class="zom-airdrop-title">ZOM Airdrop Form</h1>
+                    <form class="text-center" role="form" id="airdrop-form" novalidate="novalidate">
+                        <div class="form-group">
+                            <input type="text" class="form-control yazom-input" name="email" id="email" placeholder="Email Address">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control yazom-input" name="wallet_address" placeholder="Ethereum Wallet Address">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn sign-in-btn btn-block" id="btn_submit">SUBMIT</button>
+                        </div>
+                        <span class="airdrop-submitted success-msg"></span>
+                    </form>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--include footer-->
+<?php include 'inc/footer.php'; ?>
+
+<!--include js-->
+<?php include 'inc/includes_bottom.php'; ?>
+<script type="application/javascript">
+    var isMobile = false;
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Opera Mobile|Kindle|Windows Phone|PSP|AvantGo|Atomic Web Browser|Blazer|Chrome Mobile|Dolphin|Dolfin|Doris|GO Browser|Jasmine|MicroB|Mobile Firefox|Mobile Safari|Mobile Silk|Motorola Internet Browser|NetFront|NineSky|Nokia Web Browser|Obigo|Openwave Mobile Browser|Palm Pre web browser|Polaris|PS Vita browser|Puffin|QQbrowser|SEMC Browser|Skyfire|Tear|TeaShark|UC Browser|uZard Web|wOSBrowser|Yandex.Browser mobile/i.test(navigator.userAgent)) isMobile = true;
+    $(document).ready(function () {
+
+        $("#airdrop-form").validate({
+            rules: {
+                email: {
+                    required: true,
+                    email: true
+                },
+                wallet_address: {
+                    required: true
+                }
+            },
+            messages: {
+                email: {
+                    required: "What is your email?",
+                    email: "Please enter a valid email address.",
+                },
+                wallet_address: "Enter your ethereum wallet address here.",
+            },
+            errorPlacement: function (error, element) {
+                element.attr('data-original-title', error.text());
+                $('.error').tooltip({
+                    placement: isMobile ? "top" : "left",
+                    trigger: "focus"
+                });
+            },
+            success: function (element) {
+                $('.valid').tooltip('hide');
+            }
+        });
+
+        //Click to save button an user Register
+        $(document).off("click", "#btn_submit").on('click', "#btn_submit", function () {
+            var ele = $(this);
+            if ($("#airdrop-form").valid()) {
+                $.ajax({
+                    data: $("#airdrop-form").serialize(),
+                    url: "<?php echo base_url(); ?>index.php?login/zom_airdrop_submit",
+                    type: 'POST',
+                    dataType: 'json',
+                    beforeSend: function () {
+                        $(".error").html("");
+                        $(".success-msg").html("");
+                        ele.prop("disabled", true);
+                        ele.html('<i class="fa fa-spinner fa-spin"></i> Sending...');
+                    },
+                    success: function (result) {
+                        ele.prop("disabled", false);
+                        ele.html('SUBMIT');
+                        if (result['success'] === true) {
+                            $(".success-msg").html('<img src="<?php echo base_url("assets/yazom/img/icon/check.svg"); ?>">&nbsp;' + result['message']);
+                            $('#airdrop-form')[0].reset();
+                        } else if (result['exists'] === true) {
+                            $(".success-msg").html('<img src="<?php echo base_url("assets/yazom/img/icon/stop.svg"); ?>">&nbsp;' + result['message']);
+                        }
+                    },
+                    error: function (xhr, status, error) {
+                        alert("Something went wrong! Please refresh the page.");
+                    }
+                });
+            }
+        });
+
+    });
+</script>
